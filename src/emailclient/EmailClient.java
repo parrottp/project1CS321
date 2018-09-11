7cd8458026a1bd9c0bf218795e226612651b56d7
Payton <parrott26@gmail.comm>
2018-09-11 15:06:10 -0500

    got basic layout for email class

--- src/emailclient/EmailClient.java
+++ src/emailclient/EmailClient.java
@@ -3,30 +3,38 @@
  * 
  */
 package emailclient;
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-
-
 
 
 public class EmailClient 
 {
+    private String sender;
+    private String messageBody;
+    private String recipient;
+    private String carbonCopy;
     
-   public void messageBody(String[] body) throws IOException
+    
+    
+   public void setSender(String s)
    {
-        BufferedReader in = new BufferedReader(
-            new InputStreamReader(System.in));
-        
-        String s;
- 
-        while ((s = in.readLine()) != null) 
-        {
-            System.out.println(s);
-        }
+        sender = s;
    }
+   
+   public String getSender()
+   {
+       return sender;
+   }
+   
+   public void setRecipient(String r)
+   {
+       recipient = r;
+   }
+   
+   public String ()
+   {
+       return recipient;
+   }
+   
+   
           
     
     
